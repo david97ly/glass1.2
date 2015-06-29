7daf0adefc8878bfa39a18f68a6eb948b9a30a06
David Cortez Rivera <david97ly@hotmail.com>
2015-06-29 02:43:58 -0600

    veamos que tal funciona el mapa

--- glass/static/js/maps.js
+++ glass/static/js/maps.js
@@ -1,3 +1,31 @@
 $(function() {
-	alert("hola si funciona");
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(getCoords,getError);
+		
+	}else{
+		  initialize(13.7761745,-90.0330749);	
+	}
+	
+	function getCoords(position) {
+		var lat = position.coords.latutude;
+		var lng = Position.coords.longitude;
+		
+		initialize(lat,lng);
+	}
+	
+	function getError(err) {
+         initialize(13.7761745,-90.0330749);		
+	}
+	
+	function initialize(lat,lng) {
+		var latlng = new google.maps.LatLng(lat,lng);
+		var mapSettings = {
+			center:latlng,
+			zoom: 15,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+		
+		map = new google.maps.Map($('#mapa').get(0), mapSettings);
+	}
+	
 });
